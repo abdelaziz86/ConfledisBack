466fa42247dc4deea468b606cab61e9ef325b002
abdelaziz86 <62603535+abdelaziz86@users.noreply.github.com>
2023-07-17 17:49:51 +0200

    second

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 const express = require('express');
-const http = require('http')
-const mongo = require('mongoose'); 
+const http = require('http');
 const mongoconnection = require('./config/mongoconnection.json'); 
 const bodyParser = require("body-parser")
-var path = require("path");
-const {add} = require("./controller/chatController")
+var path = require("path"); 
 
 
 // =========   DATABASE ============
-mongo.connect(mongoconnection.url, {
+
+
+const mongoose = require("mongoose");
+
+const MONGODB_URI =
+  "mongodb+srv://abdelaziz:E5rYiu1yFAFF1jUU@cluster0.m5hcwfc.mongodb.net/?retryWrites=true&w=majority";
+const DB_NAME = "accreditation";
+
+mongoose
+  .connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-    .then(() => {
-        console.log("DataBase Connected");
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-
+    dbName: DB_NAME,
+  })
+  .then(() => {
+    console.log("MongoDB Atlas Connected");
+    // Additional code or server setup can be added here
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB Atlas:", err);
+  });
 
 
 var app = express();
@@ -33,8 +41,8 @@ app.use(bodyParser.json());
 
 // ======== ROUTES =================
 
-var abonneRouter = require("./routes/produit");
+var produitRouter = require("./routes/produit");
 app.use("/produit", produitRouter);    
 // ======== SERVER =================
-
+const server = http.createServer(app);
 server.listen(3000, () => console.log('server'))
